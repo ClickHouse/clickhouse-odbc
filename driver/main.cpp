29a1a6b9e3bba1b17803bed140a596b1582dbe3e
Artemkin Pavel <artmkin@gmail.com>
2017-04-03 15:58:39 +0500

    process DSN deletion

--- driver/main.cpp
+++ driver/main.cpp
@@ -2,6 +2,9 @@
 
 #if defined (_win_)
 
+#define strcpy strcpy_s
+#define stricmp _stricmp
+
 #include <algorithm>
 
 #include <odbcinst.h>
@@ -12,40 +15,273 @@
 #   define INTFUNC  __stdcall
 #endif /* INTFUNC */
 
-#define MAXPGPATH					1024
+namespace
+{
+#define MEDIUM_REGISTRY_LEN			256 /* normal size for * user,database,etc. */
+#define SMALL_REGISTRY_LEN			10	/* for 1/0 settings */
 
-#define MAXKEYLEN		(32+1)	/* Max keyword length */
-#define	MAXDSNAME	(32+1)	/* Max data source name length */
+#define MAXPGPATH	1024
+/// Max keyword length
+#define MAXKEYLEN	(32+1)	
+/// Max data source name length
+#define	MAXDSNAME	(32+1)
 
-HINSTANCE NEAR s_hModule;		/* Saved module handle. */
+#define INI_KDESC			"Description"	/* Data source description */
+#define INI_DATABASE		"Database"	    /* Database Name */
+#define INI_SERVER			"Servername"	/* Name of Server  running the ClickHouse service */
+#define INI_UID				"UID"		    /* Default User Name */
+#define INI_USERNAME		"Username"	    /* Default User Name */
+#define INI_PASSWORD		"Password"	    /* Default Password */
+#define INI_PORT			"Port"	        /* Port on which the ClickHouse is listening */
+#define INI_READONLY		"ReadOnly"	    /* Database is read only */
+#define INI_PROTOCOL		"Protocol"	    /* What protocol (6.2) */
+
+#define ABBR_PROTOCOL		"A1"
+#define ABBR_READONLY		"A0"
+
+#define SPEC_SERVER			"server"
+
+/// Saved module handle.
+HINSTANCE NEAR      s_hModule;		
 
 /* Globals */
+
+
+/*	Structure to hold all the connection attributes for a specific
+connection (used for both registry and file, DSN and DRIVER)
+*/
+struct ConnInfo
+{
+    char		dsn[MEDIUM_REGISTRY_LEN];
+    char		desc[MEDIUM_REGISTRY_LEN];
+    char		drivername[MEDIUM_REGISTRY_LEN];
+    char		server[MEDIUM_REGISTRY_LEN];
+    char		database[MEDIUM_REGISTRY_LEN];
+    char		username[MEDIUM_REGISTRY_LEN];
+    char		password[MEDIUM_REGISTRY_LEN];
+    char		port[SMALL_REGISTRY_LEN];
+    char		sslmode[16];
+    char		onlyread[SMALL_REGISTRY_LEN];
+    char		fake_oid_index[SMALL_REGISTRY_LEN];
+    char		show_oid_column[SMALL_REGISTRY_LEN];
+    char		row_versioning[SMALL_REGISTRY_LEN];
+    char		show_system_tables[SMALL_REGISTRY_LEN];
+    char		translation_dll[MEDIUM_REGISTRY_LEN];
+    char		translation_option[SMALL_REGISTRY_LEN];
+    char		focus_password;
+    char		conn_settings[MEDIUM_REGISTRY_LEN];
+    signed char	disallow_premature = -1;
+    signed char	allow_keyset = -1;
+    signed char	updatable_cursors = 0;
+    signed char	lf_conversion = -1;
+    signed char	true_is_minus1 = -1;
+    signed char	int8_as = -101;
+    signed char	bytea_as_longvarbinary = -1;
+    signed char	use_server_side_prepare = -1;
+    signed char	lower_case_identifier = -1;
+    signed char	rollback_on_error = -1;
+    signed char	force_abbrev_connstr = -1;
+    signed char	bde_environment = -1;
+    signed char	fake_mss = -1;
+    signed char	cvt_null_date_string = -1;
+    signed char	autocommit_public = SQL_AUTOCOMMIT_ON;
+    signed char	accessible_only = -1;
+    signed char	ignore_round_trip_time = -1;
+    signed char	disable_keepalive = -1;
+    signed char	gssauth_use_gssapi = -1;
+};
+
 /* NOTE:  All these are used by the dialog procedures */
-typedef struct tagSETUPDLG
+struct SetupDialogData
 {
-    HWND		hwndParent;		/* Parent window handle */
-    LPCSTR		lpszDrvr;		/* Driver description */
-    //ConnInfo	ci;
-    char		szDSN[MAXDSNAME];		/* Original data source name */
-    BOOL		fNewDSN;		/* New data source flag */
-    BOOL		fDefault;		/* Default data source flag */
+    HWND		hwnd_parent;	/// Parent window handle
+    LPCSTR		driver_name;	/// Driver description
+    ConnInfo	ci;
+    char		dsn[MAXDSNAME]; /// Original data source name
+    bool		is_new_dsn;		/// New data source flag
+    bool		is_default;	    /// Default data source flag
+};
 
-}	SETUPDLG, FAR * LPSETUPDLG;
-
-extern "C" 
+BOOL
+copyAttributes(ConnInfo *ci, const char * attribute, const char * value)
 {
+	BOOL	found = TRUE;
 
-void INTFUNC
-ParseAttributes(LPCSTR lpszAttributes, LPSETUPDLG lpsetupdlg)
-{
+	if (stricmp(attribute, "DSN") == 0)
+		strcpy(ci->dsn, value);
+
+	else if (stricmp(attribute, "driver") == 0)
+		strcpy(ci->drivername, value);
+
+	else if (stricmp(attribute, INI_KDESC) == 0)
+		strcpy(ci->desc, value);
+
+	else if (stricmp(attribute, INI_DATABASE) == 0)
+		strcpy(ci->database, value);
+
+	else if (stricmp(attribute, INI_SERVER) == 0 || stricmp(attribute, SPEC_SERVER) == 0)
+		strcpy(ci->server, value);
+
+	else if (stricmp(attribute, INI_USERNAME) == 0 || stricmp(attribute, INI_UID) == 0)
+		strcpy(ci->username, value);
+
+	//else if (stricmp(attribute, INI_PASSWORD) == 0 || stricmp(attribute, "pwd") == 0)
+	//	ci->password = decode_or_remove_braces(value);
+
+	else if (stricmp(attribute, INI_PORT) == 0)
+		strcpy(ci->port, value);
+
+	else if (stricmp(attribute, INI_READONLY) == 0 || stricmp(attribute, ABBR_READONLY) == 0)
+		strcpy(ci->onlyread, value);
 #if 0
+	else if (stricmp(attribute, INI_PROTOCOL) == 0 || stricmp(attribute, ABBR_PROTOCOL) == 0)
+	{
+		char * ptr;
+		/*
+		 * The first part of the Protocol used to be "6.2", "6.3" or
+		 * "7.4" to denote which protocol version to use. Nowadays we
+		 * only support the 7.4 protocol, also known as the protocol
+		 * version 3. So just ignore the first part of the string,
+		 * parsing only the rollback_on_error value.
+		 */
+		ptr = (char*)strchr(value, '-');
+		if (ptr)
+		{
+			if ('-' != *value)
+			{
+				*ptr = '\0';
+				/* ignore first part */
+			}
+			ci->rollback_on_error = atoi(ptr + 1);
+		}
+	}
+
+	else if (stricmp(attribute, INI_SHOWOIDCOLUMN) == 0 || stricmp(attribute, ABBR_SHOWOIDCOLUMN) == 0)
+		strcpy(ci->show_oid_column, value);
+
+	else if (stricmp(attribute, INI_FAKEOIDINDEX) == 0 || stricmp(attribute, ABBR_FAKEOIDINDEX) == 0)
+		strcpy(ci->fake_oid_index, value);
+
+	else if (stricmp(attribute, INI_ROWVERSIONING) == 0 || stricmp(attribute, ABBR_ROWVERSIONING) == 0)
+		strcpy(ci->row_versioning, value);
+
+	else if (stricmp(attribute, INI_SHOWSYSTEMTABLES) == 0 || stricmp(attribute, ABBR_SHOWSYSTEMTABLES) == 0)
+		strcpy(ci->show_system_tables, value);
+
+	else if (stricmp(attribute, INI_CONNSETTINGS) == 0 || stricmp(attribute, ABBR_CONNSETTINGS) == 0)
+	{
+		/* We can use the conn_settings directly when they are enclosed with braces */
+		if ('{' == *value)
+		{
+			size_t	len;
+
+			len = strlen(value + 1);
+			if (len > 0 && '}' == value[len])
+				len--;
+			STRN_TO_NAME(ci->conn_settings, value + 1, len);
+		}
+		else
+			ci->conn_settings = decode(value);
+	}
+	else if (stricmp(attribute, INI_DISALLOWPREMATURE) == 0 || stricmp(attribute, ABBR_DISALLOWPREMATURE) == 0)
+		ci->disallow_premature = atoi(value);
+	else if (stricmp(attribute, INI_UPDATABLECURSORS) == 0 || stricmp(attribute, ABBR_UPDATABLECURSORS) == 0)
+		ci->allow_keyset = atoi(value);
+	else if (stricmp(attribute, INI_LFCONVERSION) == 0 || stricmp(attribute, ABBR_LFCONVERSION) == 0)
+		ci->lf_conversion = atoi(value);
+	else if (stricmp(attribute, INI_TRUEISMINUS1) == 0 || stricmp(attribute, ABBR_TRUEISMINUS1) == 0)
+		ci->true_is_minus1 = atoi(value);
+	else if (stricmp(attribute, INI_INT8AS) == 0)
+		ci->int8_as = atoi(value);
+	else if (stricmp(attribute, INI_BYTEAASLONGVARBINARY) == 0 || stricmp(attribute, ABBR_BYTEAASLONGVARBINARY) == 0)
+		ci->bytea_as_longvarbinary = atoi(value);
+	else if (stricmp(attribute, INI_USESERVERSIDEPREPARE) == 0 || stricmp(attribute, ABBR_USESERVERSIDEPREPARE) == 0)
+		ci->use_server_side_prepare = atoi(value);
+	else if (stricmp(attribute, INI_LOWERCASEIDENTIFIER) == 0 || stricmp(attribute, ABBR_LOWERCASEIDENTIFIER) == 0)
+		ci->lower_case_identifier = atoi(value);
+	else if (stricmp(attribute, INI_GSSAUTHUSEGSSAPI) == 0 || stricmp(attribute, ABBR_GSSAUTHUSEGSSAPI) == 0)
+		ci->gssauth_use_gssapi = atoi(value);
+	else if (stricmp(attribute, INI_KEEPALIVETIME) == 0 || stricmp(attribute, ABBR_KEEPALIVETIME) == 0)
+		ci->keepalive_idle = atoi(value);
+	else if (stricmp(attribute, INI_KEEPALIVEINTERVAL) == 0 || stricmp(attribute, ABBR_KEEPALIVEINTERVAL) == 0)
+		ci->keepalive_interval = atoi(value);
+#ifdef	USE_LIBPQ
+	else if (stricmp(attribute, INI_PREFERLIBPQ) == 0 || stricmp(attribute, ABBR_PREFERLIBPQ) == 0)
+		ci->prefer_libpq = atoi(value);
+#endif /* USE_LIBPQ */
+	else if (stricmp(attribute, INI_SSLMODE) == 0 || stricmp(attribute, ABBR_SSLMODE) == 0)
+	{
+		switch (value[0])
+		{
+			case SSLLBYTE_ALLOW:
+				strcpy(ci->sslmode, SSLMODE_ALLOW);
+				break;
+			case SSLLBYTE_PREFER:
+				strcpy(ci->sslmode, SSLMODE_PREFER);
+				break;
+			case SSLLBYTE_REQUIRE:
+				strcpy(ci->sslmode, SSLMODE_REQUIRE);
+				break;
+			case SSLLBYTE_VERIFY:
+				switch (value[1])
+				{
+					case 'f':
+						strcpy(ci->sslmode, SSLMODE_VERIFY_FULL);
+						break;
+					case 'c':
+						strcpy(ci->sslmode, SSLMODE_VERIFY_CA);
+						break;
+					default:
+						strcpy(ci->sslmode, value);
+				}
+				break;
+			case SSLLBYTE_DISABLE:
+			default:
+				strcpy(ci->sslmode, SSLMODE_DISABLE);
+				break;
+		}
+	}
+	else if (stricmp(attribute, INI_ABBREVIATE) == 0)
+		unfoldCXAttribute(ci, value);
+#ifdef	_HANDLE_ENLIST_IN_DTC_
+	else if (stricmp(attribute, INI_XAOPT) == 0)
+		ci->xa_opt = atoi(value);
+#endif /* _HANDLE_ENLIST_IN_DTC_ */
+	else if (stricmp(attribute, INI_EXTRAOPTIONS) == 0)
+	{
+		UInt4	val1 = 0, val2 = 0;
+
+		if ('+' == value[0])
+		{
+			sscanf(value + 1, "%x-%x", &val1, &val2);
+			add_removeExtraOptions(ci, val1, val2);
+		}
+		else if ('-' == value[0])
+		{
+			sscanf(value + 1, "%x", &val2);
+			add_removeExtraOptions(ci, 0, val2);
+		}
+		else
+		{
+			setExtraOptions(ci, value, hex_format);
+		}
+	}
+#endif
+	else
+		found = FALSE;
+
+	return found;
+}
+
+static void parseAttributes(LPCSTR lpszAttributes, SetupDialogData * lpsetupdlg)
+{
     LPCSTR		lpsz;
     LPCSTR		lpszStart;
     char		aszKey[MAXKEYLEN];
     int			cbKey;
     char		value[MAXPGPATH];
 
-    CC_conninfo_init(&(lpsetupdlg->ci), COPY_GLOBALS);
+    //CC_conninfo_init(&(lpsetupdlg->ci), COPY_GLOBALS);
 
     for (lpsz = lpszAttributes; *lpsz; lpsz++)
     {
@@ -77,30 +313,28 @@ ParseAttributes(LPCSTR lpszAttributes, LPSETUPDLG lpsetupdlg)
         /* lpsetupdlg->aAttr[iElement].fSupplied = TRUE; */
         memcpy(value, lpszStart, std::min<size_t>(lpsz - lpszStart + 1, MAXPGPATH));
 
-        //mylog("aszKey='%s', value='%s'\n", aszKey, value);
-
         /* Copy the appropriate value to the conninfo  */
-        if (!copyAttributes(&lpsetupdlg->ci, aszKey, value))
-            copyCommonAttributes(&lpsetupdlg->ci, aszKey, value);
+        copyAttributes(&lpsetupdlg->ci, aszKey, value);
+
+        //if (!copyAttributes(&lpsetupdlg->ci, aszKey, value))
+        //    copyCommonAttributes(&lpsetupdlg->ci, aszKey, value);
     }
-#endif
 }
 
-BOOL INTFUNC
-SetDSNAttributes(HWND hwndParent, LPSETUPDLG lpsetupdlg, DWORD *errcode)
+static bool setDSNAttributes(HWND hwndParent, SetupDialogData * lpsetupdlg, DWORD * errcode)
 {
     LPCSTR		lpszDSN;		/* Pointer to data source name */
 
-    lpszDSN = "2";//lpsetupdlg->ci.dsn;
+    lpszDSN = lpsetupdlg->ci.dsn;
 
     if (errcode)
         *errcode = 0;
     /* Validate arguments */
-    //if (lpsetupdlg->fNewDSN && !*lpsetupdlg->ci.dsn)
-    //    return FALSE;
+    if (lpsetupdlg->is_new_dsn && !*lpsetupdlg->ci.dsn)
+        return FALSE;
 
     /* Write the data source name */
-    if (!SQLWriteDSNToIni(lpszDSN, lpsetupdlg->lpszDrvr))
+    if (!SQLWriteDSNToIni(lpszDSN, lpsetupdlg->driver_name))
     {
         RETCODE	ret = SQL_ERROR;
         DWORD	err = SQL_ERROR;
@@ -109,7 +343,7 @@ SetDSNAttributes(HWND hwndParent, LPSETUPDLG lpsetupdlg, DWORD *errcode)
         ret = SQLInstallerError(1, &err, szMsg, sizeof(szMsg), NULL);
         if (hwndParent)
         {
-            char		szBuf[MAXPGPATH];
+            //char		szBuf[MAXPGPATH];
 
             if (SQL_SUCCESS != ret)
             {
@@ -129,10 +363,16 @@ SetDSNAttributes(HWND hwndParent, LPSETUPDLG lpsetupdlg, DWORD *errcode)
     //writeDSNinfo(&lpsetupdlg->ci);
 
     /* If the data source name has changed, remove the old name */
-    //if (lstrcmpi(lpsetupdlg->szDSN, lpsetupdlg->ci.dsn))
-    //    SQLRemoveDSNFromIni(lpsetupdlg->szDSN);
+    if (lstrcmpi(lpsetupdlg->dsn, lpsetupdlg->ci.dsn))
+        SQLRemoveDSNFromIni(lpsetupdlg->dsn);
+
     return TRUE;
 }
+
+} // namespace 
+
+extern "C" 
+{
 
 BOOL CALLBACK
 ConfigDSN(HWND hwnd,
@@ -142,42 +382,44 @@ ConfigDSN(HWND hwnd,
 {
     BOOL fSuccess = FALSE;
     GLOBALHANDLE hglbAttr;
-    LPSETUPDLG lpsetupdlg;
+    SetupDialogData * lpsetupdlg;
 
     /* Allocate attribute array */
-    hglbAttr = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, sizeof(SETUPDLG));
+    hglbAttr = GlobalAlloc(GMEM_MOVEABLE | GMEM_ZEROINIT, sizeof(SetupDialogData));
     if (!hglbAttr)
         return FALSE;
-    lpsetupdlg = (LPSETUPDLG)GlobalLock(hglbAttr);
+    lpsetupdlg = (SetupDialogData *)GlobalLock(hglbAttr);
     /* Parse attribute string */
     if (lpszAttributes)
-        ParseAttributes(lpszAttributes, lpsetupdlg);
+        parseAttributes(lpszAttributes, lpsetupdlg);
 
     /* Save original data source name */
-    //if (lpsetupdlg->ci.dsn[0])
-    //    lstrcpy(lpsetupdlg->szDSN, lpsetupdlg->ci.dsn);
-    //else
-    //    lpsetupdlg->szDSN[0] = '\0';
+    if (lpsetupdlg->ci.dsn[0])
+        lstrcpy(lpsetupdlg->dsn, lpsetupdlg->ci.dsn);
+    else
+        lpsetupdlg->dsn[0] = '\0';
 
     /* Remove data source */
     if (ODBC_REMOVE_DSN == fRequest)
     {
         /* Fail if no data source name was supplied */
-        //if (!lpsetupdlg->ci.dsn[0])
-        //    fSuccess = FALSE;
+        if (!lpsetupdlg->ci.dsn[0])
+            fSuccess = FALSE;
 
         /* Otherwise remove data source from ODBC.INI */
-        //else
-        //    fSuccess = SQLRemoveDSNFromIni(lpsetupdlg->ci.dsn);
+        else
+            fSuccess = SQLRemoveDSNFromIni(lpsetupdlg->ci.dsn);
     }
     /* Add or Configure data source */
     else
     {
         /* Save passed variables for global access (e.g., dialog access) */
-        lpsetupdlg->hwndParent = hwnd;
-        lpsetupdlg->lpszDrvr = lpszDriver;
-        lpsetupdlg->fNewDSN = (ODBC_ADD_DSN == fRequest);
-        //lpsetupdlg->fDefault = !lstrcmpi(lpsetupdlg->ci.dsn, INI_DSN);
+        lpsetupdlg->hwnd_parent = hwnd;
+        lpsetupdlg->driver_name = lpszDriver;
+        lpsetupdlg->is_new_dsn = (ODBC_ADD_DSN == fRequest);
+        lpsetupdlg->is_default = true;//!lstrcmpi(lpsetupdlg->ci.dsn, INI_DSN);
+
+        strcpy(lpsetupdlg->ci.dsn, "2");
 
         /*
         * Display the appropriate dialog (if parent window handle
@@ -186,16 +428,17 @@ ConfigDSN(HWND hwnd,
         if (hwnd)
         {
             /* Display dialog(s) */
-            fSuccess = true; /*(IDOK == DialogBoxParam(s_hModule,
+            fSuccess = setDSNAttributes(hwnd, lpsetupdlg, NULL);
+            /*fSuccess = (IDOK == DialogBoxParam(s_hModule,
                                                MAKEINTRESOURCE(DLG_CONFIG),
                                                hwnd,
                                                ConfigDlgProc,
                                                (LPARAM)lpsetupdlg)); */
         }
-        //else if (lpsetupdlg->ci.dsn[0])
-            fSuccess = SetDSNAttributes(hwnd, lpsetupdlg, NULL);
-        //else
-        //    fSuccess = TRUE;
+        else if (lpsetupdlg->ci.dsn[0])
+            fSuccess = setDSNAttributes(hwnd, lpsetupdlg, NULL);
+        else
+            fSuccess = TRUE;
     }
 
     GlobalUnlock(hglbAttr);
