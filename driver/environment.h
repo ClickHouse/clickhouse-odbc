10e2e3dce761b51119378140445430eddf0c9409
deem0n <dima@yasp.com>
2017-10-26 14:19:47 +0300

    Fixed problem in multiuser environments when only one user can really use clickhouse-odbc driver. (#39)

--- driver/environment.h
+++ driver/environment.h
@@ -3,6 +3,8 @@
 #include "diagnostics.h"
 
 #include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
 
 #include <map>
 #include <stdexcept>
@@ -34,7 +36,15 @@ struct Environment
     Environment()
     {
 #if defined (_unix_)
+        struct passwd *pw;
+        uid_t uid;
         std::string stderr_path = "/tmp/clickhouse-odbc-stderr";
+        uid = geteuid ();
+        pw = getpwuid (uid);
+        if (pw)
+        {
+            stderr_path += "." + std::string(pw->pw_name);
+        }
         if (!freopen(stderr_path.c_str(), "w", stderr))
             throw std::logic_error("Cannot freopen stderr.");
 #endif
