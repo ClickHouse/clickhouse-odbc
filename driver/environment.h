32364ca6b8999fcded7e7627b8f0fb940af49e5f
Artemkin Pavel <artmkin@gmail.com>
2017-10-30 10:26:54 +0500

    fix build on windows

--- driver/environment.h
+++ driver/environment.h
@@ -2,10 +2,6 @@
 
 #include "diagnostics.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <pwd.h>
-
 #include <map>
 #include <stdexcept>
 
@@ -33,41 +29,10 @@ struct TypeInfo
 
 struct Environment
 {
-    Environment()
-    {
-#if defined (_unix_)
-        struct passwd *pw;
-        uid_t uid;
-        std::string stderr_path = "/tmp/clickhouse-odbc-stderr";
-        uid = geteuid ();
-        pw = getpwuid (uid);
-        if (pw)
-        {
-            stderr_path += "." + std::string(pw->pw_name);
-        }
-        if (!freopen(stderr_path.c_str(), "w", stderr))
-            throw std::logic_error("Cannot freopen stderr.");
-#endif
-    }
+    Environment();
+    ~Environment();
 
-    const std::map<std::string, TypeInfo> types_info =
-    {
-        {"UInt8",       TypeInfo{"TINYINT",   true,    SQL_TINYINT,         3,  1}},
-        {"UInt16",      TypeInfo{"SMALLINT",  true,    SQL_SMALLINT,        5,  2}},
-        {"UInt32",      TypeInfo{"INT",       true,    SQL_INTEGER,         10, 4}},
-        {"UInt64",      TypeInfo{"BIGINT",    true,    SQL_BIGINT,          19, 8}},
-        {"Int8",        TypeInfo{"TINYINT",   false,   SQL_TINYINT,         3,  1}},
-        {"Int16",       TypeInfo{"SMALLINT",  false,   SQL_SMALLINT,        5,  2}},
-        {"Int32",       TypeInfo{"INT",       false,   SQL_INTEGER,         10, 4}},
-        {"Int64",       TypeInfo{"BIGINT",    false,   SQL_BIGINT,          20, 8}},
-        {"Float32",     TypeInfo{"REAL",      false,   SQL_REAL,            7,  4}},
-        {"Float64",     TypeInfo{"DOUBLE",    false,   SQL_DOUBLE,          15, 8}},
-        {"String",      TypeInfo{"TEXT",      true,    SQL_VARCHAR,         0xFFFFFF, (1 << 20)}},
-        {"FixedString", TypeInfo{"TEXT",      true,    SQL_VARCHAR,         0xFFFFFF, (1 << 20)}},
-        {"Date",        TypeInfo{"DATE",      true,    SQL_TYPE_DATE,       10, 6}},
-        {"DateTime",    TypeInfo{"TIMESTAMP", true,    SQL_TYPE_TIMESTAMP,  19, 16}},
-        {"Array",       TypeInfo{"TEXT",      true,    SQL_VARCHAR,         0xFFFFFF, (1 << 20)}},
-    };
+    static const std::map<std::string, TypeInfo> types_info;
 
     SQLUINTEGER metadata_id = SQL_FALSE;
     int odbc_version = SQL_OV_ODBC3_80;
