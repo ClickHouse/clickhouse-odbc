d7ec27d11d4563df6db5ff1fc33993425eb92aa9
proller <proller@github.com>
2018-05-17 16:15:58 +0300

    fix

--- test/mssql.linked.server.sql
+++ test/mssql.linked.server.sql
@@ -1,4 +1,5 @@
 -- sqlcmd -S .\MSSQLSERVER -i mssql.linked.server.sql
+-- sqlcmd -i mssql.linked.server.sql
 
 EXEC master.dbo.sp_addlinkedserver
         @server = N'clickhouse_link_test'
@@ -6,6 +7,6 @@ EXEC master.dbo.sp_addlinkedserver
        ,@provider=N'MSDASQL'
        ,@provstr=N'Driver={ClickHouse Unicode};SERVER=ch1.setun.net;DATABASE=test;'
 
-EXEC sp_serveroption  'clickhouse_link_test','rpc','true';
+EXEC sp_serveroption 'clickhouse_link_test','rpc','true';
 EXEC sp_serveroption 'clickhouse_link_test','rpc out','true';
 EXEC('select name from system.databases;') at [clickhouse_link_test];
