c2feb02d30f17912077555e8610c93cb5c7f040a
proller <proller@github.com>
2018-05-22 20:43:44 +0300

    Add test perl script

---
+++ test/test.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+# sudo apt install -y libdbd-odbc-perl
+
+use 5.16.0;
+use DBI;
+use Data::Dumper;
+
+my $config = {DSN => $ENV{DSN} || 'clickhouse_localhost'};
+my $dbh = DBI->connect('dbi:ODBC:DSN=' . $config->{DSN}, $config->{'user'}, $config->{'password'});
+
+sub prepare_execute_hash ($) {
+    my $sth = $dbh->prepare($_[0]);
+    #$sth->{LongReadLen} = 1024 * 1024;
+    $sth->{LongReadLen} = 255*255;
+    $sth->{LongTruncOk} = 1;
+    return undef unless $sth->execute();
+    my $ret;
+    while (my $hash_ref = $sth->fetchrow_hashref()) {
+        push @$ret, $hash_ref;
+    }
+    return $ret;
+}
+
+say Data::Dumper::Dumper prepare_execute_hash 'SELECT 1+1';
+
+say Data::Dumper::Dumper prepare_execute_hash 'SELECT * FROM system.build_options';
