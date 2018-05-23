8cce0b55606883cba8e977cf51bc0cfa48cc7b27
proller <proller@github.com>
2018-05-23 19:00:58 +0300

    wip

---
+++ test/test_all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# env CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=`which clang++-6.0` -DCMAKE_C_COMPILER=`which clang-6.0`" sh -x ./test_all.sh
+
+cd ..
+for compiler in clang gcc; do
+    if [ "$compiler" = "clang" ]; then
+        CMAKE_COMPILER_FLAGS="-DCMAKE_CXX_COMPILER=`which clang++-6.0 clang++60 clang++` -DCMAKE_C_COMPILER=`which clang-6.0 clang60 clang`"
+    fi
+    if [ "$compiler" = "gcc" ]; then
+        CMAKE_COMPILER_FLAGS="-DCMAKE_CXX_COMPILER=`which g++-8 g++8 g++` -DCMAKE_C_COMPILER=`which gcc-8 gcc8 gcc`"
+    fi
+    for type in asan tsan ubsan msan release relwithdebinfo debug; do
+        mkdir -p build_${compiler}_$type
+        rm -rf build
+        ln -sf build_${compiler}_$type build
+        cd build
+        cmake .. -DCMAKE_BUILD_TYPE=$type -DTEST_DSN=${TEST_DSN=clickhouse_localhost} $CMAKE_COMPILER_FLAGS $CMAKE_FLAGS && make -j $(nproc || sysctl -n hw.ncpu || echo 4) && ctest -V
+        cd ..
+    done
+done
