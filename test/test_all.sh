62c5e19f40cfc2506ffae375e953f96d4e5b25e3
proller <proller@github.com>
2018-07-24 17:44:09 +0300

    wip

--- test/test_all.sh
+++ test/test_all.sh
@@ -3,21 +3,23 @@
 # env CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=`which clang++-6.0` -DCMAKE_C_COMPILER=`which clang-6.0`" sh -x ./test_all.sh
 
 cd ..
-for option in "" "-DUNICODE=1" ; do
-  for compiler in clang gcc; do
+  for compiler in gcc clang; do
     if [ "$compiler" = "clang" ]; then
         CMAKE_COMPILER_FLAGS="-DCMAKE_CXX_COMPILER=`which clang++-6.0 clang++60 clang++ | head -n1` -DCMAKE_C_COMPILER=`which clang-6.0 clang60 clang | head -n1`"
     fi
     if [ "$compiler" = "gcc" ]; then
         CMAKE_COMPILER_FLAGS="-DCMAKE_CXX_COMPILER=`which g++-8 g++8 g++ | head -n1` -DCMAKE_C_COMPILER=`which gcc-8 gcc8 gcc | head -n1`"
     fi
-    for type in asan tsan ubsan msan release relwithdebinfo debug; do
-        mkdir -p build_${compiler}_$type
+    for type in debug asan tsan ubsan msan release relwithdebinfo; do
+      for option in "" "-DUNICODE=1" ; do
+        build_dir=build_${compiler}_$type$option
+        mkdir -p ${build_dir}
         rm -rf build
-        ln -sf build_${compiler}_$type build
+        ln -sf ${build_dir} build
         cd build
+        rm CMakeCache.txt
         cmake .. $option -DCMAKE_BUILD_TYPE=$type -DTEST_DSN=${TEST_DSN=clickhouse_localhost} $CMAKE_COMPILER_FLAGS $CMAKE_FLAGS && make -j ${MAKEJ=$(nproc || sysctl -n hw.ncpu || echo 4)} && ctest -V
         cd ..
+      done
     done
-  done
 done
