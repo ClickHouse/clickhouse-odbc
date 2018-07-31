669d4d7fb42c732cec0a4a56d18304afffa10018
proller <proller@github.com>
2018-07-31 21:05:51 +0300

    wwip

--- driver/log.cpp
+++ driver/log.cpp
@@ -3,9 +3,11 @@
 #ifndef NDEBUG
 
 std::ofstream logstream("/tmp/clickhouse-odbc.log", std::ios::out | std::ios::app);
+std::wofstream wlogstream("/tmp/clickhouse-odbcw.log", std::ios::out | std::ios::app);
 
 #else
 
 decltype(std::cerr) & logstream = std::cerr;
+decltype(std::wcerr) & wlogstream = std::wcerr;
 
 #endif
