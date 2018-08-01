2f2ae00799f5b144c2cacb35657f78e8c0ea874a
proller <proller@github.com>
2018-08-01 12:59:39 +0300

    wip

--- test/test_all.sh
+++ test/test_all.sh
@@ -11,7 +11,7 @@ cd ..
         CMAKE_COMPILER_FLAGS="-DCMAKE_CXX_COMPILER=`which g++-8 g++-7 g++8 g++7 g++ | head -n1` -DCMAKE_C_COMPILER=`which gcc-7 gcc-8 gcc8 gcc7 gcc | head -n1`"
     fi
     for type in debug asan tsan ubsan msan release relwithdebinfo; do
-      for option in "" "-DUNICODE=1" ; do
+      for option in "-DUNICODE=1" ""; do
         build_dir=build_${compiler}_$type$option
         echo build $compiler $type $option in ${build_dir}
         mkdir -p ${build_dir}
@@ -19,7 +19,7 @@ cd ..
         ln -sf ${build_dir} build
         cd build
         rm CMakeCache.txt
-        cmake .. -G Ninja $option -DCMAKE_BUILD_TYPE=$type -DTEST_DSN=${TEST_DSN=clickhouse_localhost} -DLIB_NAME_NO_W=1 $CMAKE_COMPILER_FLAGS $CMAKE_FLAGS && cmake --build . -- -j ${MAKEJ=$(distcc -j || nproc || sysctl -n hw.ncpu || echo 4)} && ctest -V
+        cmake .. -G Ninja $option -DCMAKE_BUILD_TYPE=$type -DTEST_DSN=${TEST_DSN=clickhouse_localhost} -DBUILD_SHARED=$BUILD_SHARED -DLIB_NAME_NO_W=1 $CMAKE_COMPILER_FLAGS $CMAKE_FLAGS && cmake --build . -- -j ${MAKEJ=$(distcc -j || nproc || sysctl -n hw.ncpu || echo 4)} && ctest -V
         cd ..
       done
 exit
