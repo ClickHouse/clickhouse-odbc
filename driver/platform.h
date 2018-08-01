00de6b8c6fb25e1882c37125f4c589b70b6f9c8d
proller <proller@github.com>
2018-08-01 23:18:31 +0300

    wip

--- driver/platform.h
+++ driver/platform.h
@@ -4,77 +4,76 @@
 
 //#if __has_include("config_cmake.h") // requre c++17
 #if CMAKE_BUILD
-#include "config_cmake.h"
+#    include "config_cmake.h"
 #endif
 
 #if defined(__linux__)
-#   define _linux_ 1
+#    define _linux_ 1
 #elif defined(_WIN64)
-#   define _win64_ 1
-#   define _win32_ 1
+#    define _win64_ 1
+#    define _win32_ 1
 #elif defined(__WIN32__) || defined(_WIN32)
-#   define _win32_ 1
+#    define _win32_ 1
 #elif defined(__APPLE__)
-#   define _darwin_ 1
-#   define _unix_ 1
+#    define _darwin_ 1
+#    define _unix_ 1
 #endif
 
 #if defined(_win32_) || defined(_win64_)
-#   define _win_ 1
+#    define _win_ 1
 #endif
 
 #if defined(_linux_)
-#   define _unix_ 1
+#    define _unix_ 1
 #endif
 
 #if defined(_MSC_VER)
-#   undef NOMINMAX
-#   define NOMINMAX
-#   include <basetsd.h>
-#   define ssize_t SSIZE_T
-#   define HAVE_SSIZE_T 1
+#    undef NOMINMAX
+#    define NOMINMAX
+#    include <basetsd.h>
+#    define ssize_t SSIZE_T
+#    define HAVE_SSIZE_T 1
 #
-#   include <winsock2.h>
-#   include <ws2tcpip.h>
-#   include <windows.h>
+#    include <windows.h>
+#    include <winsock2.h>
+#    include <ws2tcpip.h>
 #endif
 
 #include <sql.h>
 #include <sqlext.h>
 #include <sqltypes.h>
 
-#if defined (_win_)
-#   ifdef UNICODE
-#       include <sqlucode.h>
+#if defined(_win_)
+#    ifdef UNICODE
+#        include <sqlucode.h>
 
-#       define strcpy wcscpy_s
-#       define stricmp _wcsicmp
+#        define strcpy wcscpy_s
+#        define stricmp _wcsicmp
 //#       define strncpy wcsncpy
-#   else
-#       define strcpy strcpy_s
-#       define stricmp _stricmp
-#   endif
+#    else
+#        define strcpy strcpy_s
+#        define stricmp _stricmp
+#    endif
 #else
 
 // Fix missing declarations in iodbc
-#if defined(_IODBCUNIX_H)
-#   if defined(UNICODE)
+#    if defined(_IODBCUNIX_H)
+#        if defined(UNICODE)
 //#      define LPCTSTR LPCWSTR
-#      define LPTSTR LPWSTR
-#   else
+#            define LPTSTR LPWSTR
+#        else
 //#      define LPCTSTR LPCSTR
-#      define LPTSTR LPSTR
-#   endif
-#   endif
+#            define LPTSTR LPSTR
+#        endif
+#    endif
+#    ifdef UNICODE
+#        define TEXT(value) L"" value
+#    else
+#        define TEXT(value) value
+#    endif
+#endif
 
 typedef std::remove_pointer<LPTSTR>::type MYTCHAR;
-
-#   ifdef UNICODE
-#      define TEXT(value)  L"" value
-#   else
-#      define TEXT(value)  value
-#   endif
-#endif
 
 /*
 #if defined (UNICODE)
@@ -91,5 +90,5 @@ typedef std::remove_pointer<LPTSTR>::type MYTCHAR;
 
 #if defined(_MSC_VER) && !defined(USE_SSL)
 // Enabled by default, but you can disable
-#   define USE_SSL 1
+#    define USE_SSL 1
 #endif
