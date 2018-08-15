6bfdf183d7e1ed110362a03388f7f52535f4067b
proller <proller@github.com>
2018-08-15 14:32:01 -0700

    windows fixes

--- vs/build_cmake.bat
+++ vs/build_cmake.bat
@@ -2,10 +2,10 @@ cd ..
 
 md build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DTEST_DSN=clickhouse_localhost -DTEST_DSN_W=clickhouse_localhost_w && cmake --build . -- /m
+cmake .. -G "Visual Studio 15 2017 Win64" -DTEST_DSN=clickhouse_localhost -DTEST_DSN_W=clickhouse_localhost_w && cmake --build . -- /m && ctest -V -C Debug
 cd ..
 
 md build32
 cd build32
-cmake .. -G "Visual Studio 15 2017" -DTEST_DSN=clickhouse_localhost -DTEST_DSN_W=clickhouse_localhost_w && cmake --build . -- /m
+cmake .. -G "Visual Studio 15 2017" -DTEST_DSN=clickhouse_localhost -DTEST_DSN_W=clickhouse_localhost_w && cmake --build . -- /m && ctest -V -C Debug
 cd ..
