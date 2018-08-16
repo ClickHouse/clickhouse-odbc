29d8ff8bdb110304ef78af79ce0a5d94a2477f57
proller <proller@github.com>
2018-08-16 21:07:24 +0300

    wip

--- test/test.pl
+++ test/test.pl
@@ -9,10 +9,10 @@ use utf8;
 use open ':encoding(utf8)', ':std';
 use DBI;
 use Data::Dumper;
-$Data::Dumper::Sortkeys=1;
+$Data::Dumper::Sortkeys = 1;
 
 my $config = {DSN => $ARGV[0] || $ENV{DSN} || 'clickhouse_localhost'};
-my $is_wide = 1 if $config->{DSN} =~ /w$/; # bad magic
+my $is_wide = 1 if $config->{DSN} =~ /w$/;    # bad magic
 
 say 'Data sources: ', join '; ', DBI->data_sources('dbi:ODBC:DSN=' . $config->{DSN},);
 
@@ -42,10 +42,10 @@ sub prepare_execute_hash ($) {
 }
 
 sub test_one_string_value($) {
-    my ($n) = @_;
-    my $row = prepare_execute_hash("SELECT '$n'")->[0];
+    my ($n)     = @_;
+    my $row     = prepare_execute_hash("SELECT '$n'")->[0];
     my ($value) = values %$row;
-    my ($key) = keys %$row;
+    my ($key)   = keys %$row;
     is $value, $n, "value eq " . $n . " " . Data::Dumper->new([$row])->Indent(0)->Terse(1)->Sortkeys(1)->Dump();
     is $key, qq{'$n'}, "header eq " . $n . " " . Data::Dumper->new([$row])->Indent(0)->Terse(1)->Sortkeys(1)->Dump();
 }
@@ -71,36 +71,42 @@ sub test_one_value_as($;$) {
 say Data::Dumper::Dumper prepare_execute_hash 'SELECT * FROM system.build_options ORDER BY length(name) ASC';
 #say Data::Dumper::Dumper prepare_execute_hash 'SELECT * FROM system.build_options ORDER BY length(name) DESC';
 #say Data::Dumper::Dumper prepare_execute_hash q{SELECT 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'};
-say Data::Dumper::Dumper prepare_execute_hash q{SELECT *, (CASE WHEN (number == 1) THEN 'o' WHEN (number == 2) THEN 'two long string' WHEN (number == 3) THEN 'r' ELSE '-' END)  FROM system.numbers LIMIT 5};
+say Data::Dumper::Dumper prepare_execute_hash
+q{SELECT *, (CASE WHEN (number == 1) THEN 'o' WHEN (number == 2) THEN 'two long string' WHEN (number == 3) THEN 'r' ELSE '-' END)  FROM system.numbers LIMIT 5};
 
 test_one_value_as(q{1+1}, 2);
 
-#TODO! 
-test_one_string_value(q{абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ}) if $is_wide;
+if ($is_wide) {
+    test_one_string_value(
+q{абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ}
+    );
 
-test_one_string_value_as(q{абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ}) if $is_wide;
-
+    test_one_string_value_as(
+q{абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ}
+    );
+}
 
 #say Data::Dumper::Dumper prepare_execute_hash 'SELECT 1, sleep(25), sleep(15), 2'; # Default timeout is 30. Maximum allowed clickhouse sleep is 30s. We want to test 30+s
 
 {
-use bigint;
-sub test_number($) {
-    my ($n) = @_;
-    my $row = prepare_execute_hash("SELECT $n")->[0];
-    is $row->{$n}, $n, "n " . $n . " " . Data::Dumper->new([$row])->Indent(0)->Terse(1)->Sortkeys(1)->Dump();
-}
+    use bigint;
 
-for my $p (8, 16, 32, 64) { # , 128
-    my @res;
-    my @range = $p == 64 ? (-1 .. -1) : (-1..1);
-    push @res, -1*($_+2**($p-1)) for @range; # -129 .. -127
-    push @res, -1+($_+2**($p-1)) for @range; # 126 .. 128
-    push @res, $_+2**$p for @range;          # 255 .. 257
-    #say join ',', @res;
-    #say Data::Dumper::Dumper prepare_execute_hash 'SELECT ' . join ',', @res;
-    test_number $_ for @res;
-}
+    sub test_number($) {
+        my ($n) = @_;
+        my $row = prepare_execute_hash("SELECT $n")->[0];
+        is $row->{$n}, $n, "n " . $n . " " . Data::Dumper->new([$row])->Indent(0)->Terse(1)->Sortkeys(1)->Dump();
+    }
+
+    for my $p (8, 16, 32, 64) {    # , 128
+        my @res;
+        my @range = $p == 64 ? (-1 .. -1) : (-1 .. 1);
+        push @res, -1 * ($_ + 2**($p - 1)) for @range;    # -129 .. -127
+        push @res, -1 + ($_ + 2**($p - 1)) for @range;    # 126 .. 128
+        push @res, $_ + 2**$p for @range;                 # 255 .. 257
+                                                          #say join ',', @res;
+                                                          #say Data::Dumper::Dumper prepare_execute_hash 'SELECT ' . join ',', @res;
+        test_number $_ for @res;
+    }
 
     say Data::Dumper::Dumper prepare_execute_hash 'SELECT 2147483647, 2147483648, 2147483647+1, 2147483647+10, 4294967295';
 
