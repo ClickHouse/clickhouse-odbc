d4794490e9219093e21702ca2ff75652e9bd7f18
proller <proller@github.com>
2018-09-17 20:28:32 +0300

    fix

--- test/test.sh
+++ test/test.sh
@@ -163,6 +163,7 @@ q 'DROP TABLE test.increment;'
 
 
 # Now disabled by default
+#q "SET allow_experimental_decimal_type = 1;"
 #q "DROP TABLE IF EXISTS test.decimal;"
 #q "CREATE TABLE IF NOT EXISTS test.decimal (a DECIMAL(9,0), b DECIMAL(18,0), c DECIMAL(38,0), d DECIMAL(9, 9), e Decimal64(18), f Decimal128(38), g Decimal32(5), h Decimal64(9), i Decimal128(18), j dec(4,2)) ENGINE = Memory;"
 #q "INSERT INTO test.decimal (a, b, c, d, e, f, g, h, i, j) VALUES (42, 42, 42, 0.42, 0.42, 0.42, 42.42, 42.42, 42.42, 42.42);"
