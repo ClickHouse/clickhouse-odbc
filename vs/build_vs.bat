93006fd449f819146227fc6c09eefe0780f5a33d
proller <proller@users.noreply.github.com>
2019-01-18 17:50:38 +0300

    Update build_vs.bat

--- vs/build_vs.bat
+++ vs/build_vs.bat
@@ -15,8 +15,8 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\
 
-::devenv /upgrade odbc64.sln
-::devenv /upgrade odbc32.sln
+devenv /upgrade odbc64.sln
+devenv /upgrade odbc32.sln
 
 msbuild /m /p:Configuration=Release odbc64.sln || exit
 msbuild /m /p:Configuration=Release odbc32.sln || exit
