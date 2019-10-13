ca3799e3d73ad2507a19786a8a715cf3160567d2
Denis Glazachev <d.glazachev@gmail.com>
2019-10-13 16:16:34 +0400

    Description for DiagnosticsContainer class

--- driver/diagnostics.h
+++ driver/diagnostics.h
@@ -25,6 +25,13 @@ class DiagnosticsRecord
 public:
 };
 
+// A class that enables other classes derived from it
+// to hold diagnostics information in a common form:
+//   a diagnostics header (record with index 0), and
+//   diagnostics records (records starting from index 1).
+// Each record is basically an AttributeContainer instance,
+// i.e., it is able to hold an integer or string values
+// under arbitrary integer keys.
 class DiagnosticsContainer {
 public:
     void fillDiag(SQLRETURN rc, const std::string& sql_status, const std::string& message, SQLINTEGER native_error_code);
