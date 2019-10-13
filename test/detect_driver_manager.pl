64a763cdf6868134e2ceaac04e06bc64e63e1924
Denis Glazachev <d.glazachev@gmail.com>
2019-10-13 21:12:29 +0400

    Detect UnixODBC vs iODBC in Perl and Python and enable tests accordingly

---
+++ test/detect_driver_manager.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use 5.16.0;
+use utf8;
+use open ':encoding(utf8)', ':std';
+use DBI;
+
+eval {
+    my $dbh = DBI->connect('dbi:ODBC:DSN=__nonexistent_dsn__', '', '', { RaiseError => 1, PrintError => 0 });
+};
+
+if ($@ =~ /\[([^\[\]]+)\]\[Driver Manager\]/) {
+    print "$1\n";
+}
