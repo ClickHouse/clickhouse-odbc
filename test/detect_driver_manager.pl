2a400a05d09c7a76485dde80d7c778cd31d8f0b4
Denis Glazachev <d.glazachev@gmail.com>
2019-10-13 22:03:42 +0400

    Better error reporting

--- test/detect_driver_manager.pl
+++ test/detect_driver_manager.pl
@@ -3,6 +3,8 @@
 use 5.16.0;
 use utf8;
 use open ':encoding(utf8)', ':std';
+use strict;
+use warnings;
 use DBI;
 
 eval {
@@ -11,4 +13,7 @@ eval {
 
 if ($@ =~ /\[([^\[\]]+)\]\[Driver Manager\]/) {
     print "$1\n";
+}
+elsif ($@) {
+    die $@;
 }
