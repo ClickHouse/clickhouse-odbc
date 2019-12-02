d7589d90ceacc892c81adcc4400bf2479fce84f4
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-12-02 06:57:20 -0500

    Removing license headers.

--- test/parameterized/parameterized/funcvalues.py
+++ test/parameterized/parameterized/funcvalues.py
@@ -1,10 +1,3 @@
-#  Copyright 2019, Altinity LTD. All Rights Reserved.
-#
-#  All information contained herein is, and remains the property
-#  of Altinity LTD. Any dissemination of this information or
-#  reproduction of this material is strictly forbidden unless
-#  prior written permission  is obtained from Altinity LTD.
-#
 import datetime
 
 from testflows.core import TestFeature, TestScenario
