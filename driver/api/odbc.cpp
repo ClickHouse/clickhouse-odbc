45e8abbe32ddeee709d57e6f95bb8e4088ea84c3
Denis Glazachev <d.glazachev@gmail.com>
2019-12-28 04:30:19 +0400

    Fix type

--- driver/api/odbc.cpp
+++ driver/api/odbc.cpp
@@ -1391,6 +1391,7 @@ SQLRETURN SQL_API EXPORTED_FUNCTION_MAYBE_W(SQLColumns)(
                  ", CAST(NULL, 'Nullable(String)') AS TABLE_SCHEM"  // 1
                  ", CAST(table, 'String') AS TABLE_NAME"            // 2
                  ", CAST(name, 'String') AS COLUMN_NAME"            // 3
+                 ", CAST(type, 'String') AS DATA_TYPE"              // 4
                  ", '' AS TYPE_NAME"        // 5
                  ", 0 AS COLUMN_SIZE"       // 6
                  ", 0 AS BUFFER_LENGTH"     // 7
