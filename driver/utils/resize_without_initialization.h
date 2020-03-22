c8c88271cd5d1268f3c6a20d8b77eea472afb69b
Denis Glazachev <d.glazachev@gmail.com>
2020-03-22 20:25:37 +0400

    Enable folly::resizeWithoutInitialization everywhere

--- driver/utils/resize_without_initialization.h
+++ driver/utils/resize_without_initialization.h
@@ -2,9 +2,5 @@
 
 #include "driver/platform/platform.h"
 
-#if defined(_MSC_VER) && _MSC_VER > 1916 // Not supported yet for Visual Studio 2019 and later.
-#   define resize_without_initialization(container, size) container.resize(size)
-#else
-#   include <folly/memory/UninitializedMemoryHacks.h>
-#   define resize_without_initialization(container, size) folly::resizeWithoutInitialization(container, size)
-#endif
+#include <folly/memory/UninitializedMemoryHacks.h>
+#define resize_without_initialization(container, size) folly::resizeWithoutInitialization(container, size)
