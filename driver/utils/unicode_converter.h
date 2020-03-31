79df8864c701b32c525847ac879c1b49a5ffee9c
Denis Glazachev <d.glazachev@gmail.com>
2020-03-31 20:41:53 +0400

    Fix compilation Fix target buffer growing calculation Implement implicit pivoting code that uses ucnv_convertEx() [slower?]

--- driver/utils/unicode_converter.h
+++ driver/utils/unicode_converter.h
@@ -515,7 +515,12 @@ inline void UnicodeConverter::convertFromPivot(
 
                 ucnv_fromUnicode(converter_, &target, target_end, &sig_source, sig_source_end, nullptr, false, &error_code);
 
-                target_symbols_written += target - target_prev;
+                const auto target_bytes_appended = target - target_prev;
+
+                if (target_bytes_appended % sizeof(CharType) != 0)
+                    throw std::runtime_error("unable to convert encoding: ucnv_fromUnicode() failed to write destination buffer to symbol boundary");
+
+                target_symbols_written += target_bytes_appended / sizeof(CharType);
 
                 // Assuming, target had enough space to hold any signature.
 
@@ -592,8 +597,126 @@ inline void convertEncoding(
     const bool trim_dest_signature
 ) {
 #if defined(WORKAROUND_ICU_USE_EXPLICIT_PIVOTING)
-    src_converter.convertToPivot(src, pivot, ensure_encoded_signature, false);
+    src_converter.convertToPivot(src, pivot, ensure_src_signature, false);
     dest_converter.convertFromPivot(make_string_view(pivot), dest, true, trim_dest_signature);
 #else
+    dest.clear();
+
+    static_assert(sizeof(PivotCharType) == sizeof(ConverterPivotWideCharType), "unable to convert encoding: unsuitable character type for the pivot");
+
+    if (sizeof(SourceCharType) != src_converter.getEncodedMinCharSize())
+        throw std::runtime_error("unable to convert encoding: unsuitable character type for the source converter");
+
+    if (sizeof(DestinationCharType) != dest_converter.getEncodedMinCharSize())
+        throw std::runtime_error("unable to convert encoding: unsuitable character type for the destination converter");
+
+    try {
+        constexpr std::size_t extra_size_reserve = 32;
+        constexpr std::size_t min_size_increment = 128;
+        constexpr std::size_t conservative_target_write_increment = 16;
+
+        const auto initial_new_size = std::min(src.size() + extra_size_reserve, min_size_increment);
+        resize_without_initialization(dest, initial_new_size);
+
+        constexpr std::size_t pivot_size = 1024;
+        resize_without_initialization(pivot, pivot_size);
+
+        auto * source = reinterpret_cast<const char *>(src.data());
+        auto * source_end = reinterpret_cast<const char *>(src.data() + src.size());
+
+        auto * target = const_cast<char *>(reinterpret_cast<const char *>(dest.c_str()));
+        auto * target_end = reinterpret_cast<const char *>(dest.c_str() + dest.size());
+
+        auto * pivot_start = const_cast<ConverterPivotWideCharType *>(reinterpret_cast<const ConverterPivotWideCharType *>(pivot.data()));
+        auto * pivot_source = pivot_start;
+        auto * pivot_target = pivot_start;
+        auto * pivot_end = reinterpret_cast<const ConverterPivotWideCharType *>(pivot.data() + pivot.size());
+
+        std::size_t target_symbols_written = 0;
+        bool dest_signature_trimmed = false;
+
+        // If signature must be prepended to the encoded string before decoding, we feed it to ucnv_convertEx() separately, to avoid heavy copying.
+        if (ensure_src_signature) {
+            auto src_no_sig = src_converter.consumeEncodedSignature(src);
+            if (src_no_sig.size() == src.size()) {
+                auto * target_prev = target;
+                auto * sig_source = reinterpret_cast<const char *>(src_converter.encoded_signature_to_prepend_.c_str());
+                auto * sig_source_end = reinterpret_cast<const char *>(src_converter.encoded_signature_to_prepend_.c_str() + src_converter.encoded_signature_to_prepend_.size());
+                UErrorCode error_code = U_ZERO_ERROR;
+
+                ucnv_convertEx(dest_converter.converter_, src_converter.converter_, &target, target_end, &sig_source, sig_source_end, pivot_start, &pivot_source, &pivot_target, pivot_end, false, false, &error_code);
+
+                const auto target_bytes_appended = target - target_prev;
+
+                if (target_bytes_appended % sizeof(DestinationCharType) != 0)
+                    throw std::runtime_error("unable to convert encoding: ucnv_convertEx() failed to write destination buffer to symbol boundary");
+
+                target_symbols_written += target_bytes_appended / sizeof(DestinationCharType);
+
+                // Assuming, target had enough space to hold any signature.
+
+                if (U_FAILURE(error_code)) {
+                    throw std::runtime_error(u_errorName(error_code));
+                }
+                else if (sig_source != sig_source_end) {
+                    throw std::runtime_error("unable to convert encoding: failed to fully decode prepended signature");
+                }
+            }
+        }
+
+        // Main streaming conversion loop.
+        while (true) {
+            auto * target_prev = target;
+            const auto * conservative_target_end_candidate = target + conservative_target_write_increment * sizeof(DestinationCharType);
+            const auto * conservative_target_end = (conservative_target_end_candidate < target_end ? conservative_target_end_candidate : target_end);
+            const auto * final_target_end = (trim_dest_signature && !dest_signature_trimmed ? conservative_target_end : target_end);
+            UErrorCode error_code = U_ZERO_ERROR;
+
+            ucnv_convertEx(dest_converter.converter_, src_converter.converter_, &target, final_target_end, &source, source_end, pivot_start, &pivot_source, &pivot_target, pivot_end, false, true, &error_code);
+
+            const auto target_bytes_appended = target - target_prev;
+
+            if (target_bytes_appended % sizeof(DestinationCharType) != 0)
+                throw std::runtime_error("unable to convert encoding: ucnv_convertEx() failed to write destination buffer to symbol boundary");
+
+            target_symbols_written += target_bytes_appended / sizeof(DestinationCharType);
+
+            while (
+                trim_dest_signature &&
+                !dest_signature_trimmed &&
+                (target_symbols_written * sizeof(DestinationCharType)) >= dest_converter.encoded_signatures_to_trim_max_size_
+            ) {
+                dest.resize(target_symbols_written); // Shrink to avoid memmove'ing unused bytes.
+                const auto consumed_symbols = dest_converter.consumeEncodedSignatureInPlace(dest); // dest is not shorter than dest_converter.encoded_signatures_to_trim_max_size_, so even 0 is valid.
+                target_symbols_written -= consumed_symbols;
+                target -= consumed_symbols * sizeof(DestinationCharType);
+                dest_signature_trimmed = (consumed_symbols == 0); // This means we may try several times, and stop only when all possible duplicate leading signatures are consumed.
+            }
+
+            if (error_code == U_BUFFER_OVERFLOW_ERROR) {
+                const std::size_t source_pending_symbols = (source_end - source) / sizeof(SourceCharType) + ((source_end - source) % sizeof(SourceCharType) == 0 ? 0 : 1);
+                const std::size_t target_new_size = target_symbols_written + source_pending_symbols;
+                const std::size_t final_new_size = std::max(target_new_size + extra_size_reserve, dest.size() + min_size_increment);
+
+                resize_without_initialization(dest, final_new_size);
+
+                target = const_cast<char *>(reinterpret_cast<const char *>(dest.c_str() + target_symbols_written));
+                target_end = reinterpret_cast<const char *>(dest.c_str() + dest.size());
+            }
+            else if (U_FAILURE(error_code)) {
+                throw std::runtime_error(u_errorName(error_code));
+            }
+            else {
+                dest.resize(target_symbols_written);
+                break;
+            }
+        }
+    }
+    catch (...) {
+        ucnv_resetToUnicode(src_converter.converter_);
+        ucnv_resetFromUnicode(dest_converter.converter_);
+        dest.clear();
+        throw;
+    }
 #endif
 }
