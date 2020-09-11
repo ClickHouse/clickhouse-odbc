f4f34a2781cbeccd6b9249565c07cc823b2b06a3
Denis Glazachev <d.glazachev@gmail.com>
2020-09-11 21:35:00 +0400

    Use binaries and configs from master for macOS (only they are reliably available right now)

--- test/deploy_and_run_clickhouse_macos.sh
+++ test/deploy_and_run_clickhouse_macos.sh
@@ -18,12 +18,13 @@ mkdir -p usr/bin etc/clickhouse-server var/lib/clickhouse
 
 echo 2. Download binaries
 # TODO: switch to actual 20.3 binaries once available. Currently, this is master branch.
-curl https://clickhouse-builds.s3.yandex.net/11870/009ddd5c8d46d1427c2775b67918e0165e50844f/clickhouse_build_check/clang-10-darwin_relwithdebuginfo_none_bundled_unsplitted_disable_False_binary/clickhouse -o usr/bin/clickhouse
-curl https://clickhouse-builds.s3.yandex.net/11870/009ddd5c8d46d1427c2775b67918e0165e50844f/clickhouse_build_check/clang-10-darwin_relwithdebuginfo_none_bundled_unsplitted_disable_False_binary/clickhouse-odbc-bridge -o usr/bin/clickhouse-odbc-bridge
+curl https://builds.clickhouse.tech/master/macos/clickhouse -o usr/bin/clickhouse
+curl https://builds.clickhouse.tech/master/macos/clickhouse-odbc-bridge -o usr/bin/clickhouse-odbc-bridge
 
 echo 3. Download configs
-curl https://raw.githubusercontent.com/ClickHouse/ClickHouse/20.5/programs/server/config.xml -o etc/clickhouse-server/config.xml
-curl https://raw.githubusercontent.com/ClickHouse/ClickHouse/20.5/programs/server/users.xml -o etc/clickhouse-server/users.xml
+# TODO: switch to the corresponding versions of configs when the binaries are fixed. Currently, this is master branch.
+curl https://raw.githubusercontent.com/ClickHouse/ClickHouse/master/programs/server/config.xml -o etc/clickhouse-server/config.xml
+curl https://raw.githubusercontent.com/ClickHouse/ClickHouse/master/programs/server/users.xml -o etc/clickhouse-server/users.xml
 
 echo 4. Setup executables
 pushd usr/bin/
