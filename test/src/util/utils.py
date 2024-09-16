c00edd4c4f5c4548b81ff98f18b04fba66da5b62
slvrtrn <hypnoash@gmail.com>
2024-09-16 19:51:00 +0200

    Remove testflows imports

--- test/src/util/utils.py
+++ test/src/util/utils.py
@@ -4,11 +4,6 @@ import time
 from contextlib import contextmanager
 
 import pyodbc
-import testflows.settings as settings
-from testflows.connect import Shell
-
-from .env import CLICKHOUSE_LOG_PATH, ODBC_DRIVER_TRACE_LOG_PATH, ODBC_DRIVER_W_TRACE_LOG_PATH, \
-    ODBC_MANAGER_TRACE_LOG_PATH
 
 LOGGER = logging.getLogger(__name__)
 
@@ -18,33 +13,35 @@ def shell_logs():
     """ClickHouse and ODBC driver logs context manager.
     """
 
-    class _Logs:
-        def __init__(self, *args):
-            self.logs = args
-
-        def read(self, timeout=None):
-            for log in self.logs:
-                log.readlines(timeout=timeout)
-
-    if not settings.debug:
-        yield None
-    else:
-        with Shell(name="clickhouse-server.log") as bash0, \
-            Shell(name="odbc-driver-trace.log") as bash1, \
-            Shell(name="odbc-driver-w-trace.log") as bash2, \
-            Shell(name="odbc-manager-trace.log") as bash3:
-
-            bash1(f"touch {ODBC_DRIVER_TRACE_LOG_PATH}")
-            bash2(f"touch {ODBC_DRIVER_W_TRACE_LOG_PATH}")
-            bash3(f"touch {ODBC_MANAGER_TRACE_LOG_PATH}")
-
-            with bash0(f"tail -f {CLICKHOUSE_LOG_PATH}", asyncronous=True, name="") as clickhouse_log, \
-                bash1(f"tail -f {ODBC_DRIVER_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_driver_log, \
-                bash2(f"tail -f {ODBC_DRIVER_W_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_driver_w_log, \
-                bash3(f"tail -f {ODBC_MANAGER_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_manager_log:
-                logs = _Logs(clickhouse_log, odbc_driver_log, odbc_driver_w_log, odbc_manager_log)
-                logs.read()
-                yield logs
+    # rewrite without testflows.Shell if needed
+    #
+    # class _Logs:
+    #     def __init__(self, *args):
+    #         self.logs = args
+    #
+    #     def read(self, timeout=None):
+    #         for log in self.logs:
+    #             log.readlines(timeout=timeout)
+    #
+    # if not settings.debug:
+    #     yield None
+    # else:
+    #     with Shell(name="clickhouse-server.log") as bash0, \
+    #         Shell(name="odbc-driver-trace.log") as bash1, \
+    #         Shell(name="odbc-driver-w-trace.log") as bash2, \
+    #         Shell(name="odbc-manager-trace.log") as bash3:
+    #
+    #         bash1(f"touch {ODBC_DRIVER_TRACE_LOG_PATH}")
+    #         bash2(f"touch {ODBC_DRIVER_W_TRACE_LOG_PATH}")
+    #         bash3(f"touch {ODBC_MANAGER_TRACE_LOG_PATH}")
+    #
+    #         with bash0(f"tail -f {CLICKHOUSE_LOG_PATH}", asyncronous=True, name="") as clickhouse_log, \
+    #             bash1(f"tail -f {ODBC_DRIVER_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_driver_log, \
+    #             bash2(f"tail -f {ODBC_DRIVER_W_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_driver_w_log, \
+    #             bash3(f"tail -f {ODBC_MANAGER_TRACE_LOG_PATH}", asyncronous=True, name="") as odbc_manager_log:
+    #             logs = _Logs(clickhouse_log, odbc_driver_log, odbc_driver_w_log, odbc_manager_log)
+    #             logs.read()
+    #             yield logs
 
 
 class PyODBCConnection:
@@ -71,10 +68,11 @@ class PyODBCConnection:
         except pyodbc.Error as exc:
             raise exc
         finally:
-            if self.logs and settings.debug:
-                # sleep 0.5 sec to let messages to be written to the logs
-                time.sleep(0.5)
-                self.logs.read(timeout=0.1)
+            pass
+            # if self.logs and settings.debug:
+            #     # sleep 0.5 sec to let messages to be written to the logs
+            #     time.sleep(0.5)
+            #     self.logs.read(timeout=0.1)
 
     def insert(self, table_name: str, values: str):
         stmt = f"INSERT INTO {table_name} VALUES {values}"
