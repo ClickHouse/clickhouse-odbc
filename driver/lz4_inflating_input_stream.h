77e299c30df40e727007825b20f100a7eda4a511
Ilya Golshtein <igolshtein@altinity.com>
2025-10-21 14:10:03 +0300

    Revert "support_compression: code cleanup"
    This reverts commit da17f18b28bededbb8da54dba806b29275a34b49.

--- driver/lz4_inflating_input_stream.h
+++ driver/lz4_inflating_input_stream.h
@@ -1,70 +1,117 @@
 #pragma once
 
-#include <istream>
 #include "Poco/BufferedStreamBuf.h"
+#include <istream>
 
 #include <lz4.h>
 #include <lz4frame.h>
 
-class LZ4InflatingStreamBuf : public Poco::BufferedStreamBuf {
+class LZ4InflatingStreamBuf : public Poco::BufferedStreamBuf
+/// This stream decompresses all data passing through it
+/// using zlib's inflate algorithm.
+/// Example:
+///     std::ifstream istr("data.gz", std::ios::binary);
+///     InflatingInputStream inflater(istr, InflatingStreamBuf::STREAM_GZIP);
+///     std::string data;
+///     inflater >> data;
+///
+/// The underlying input stream can contain more than one gzip/deflate stream.
+/// After a gzip/deflate stream has been processed, reset() can be called
+/// to inflate the next stream.
+{
 public:
-    LZ4InflatingStreamBuf(const LZ4InflatingStreamBuf &) = delete;
-    LZ4InflatingStreamBuf(LZ4InflatingStreamBuf &&) = delete;
-    LZ4InflatingStreamBuf & operator=(const LZ4InflatingStreamBuf &) = delete;
-    LZ4InflatingStreamBuf & operator=(LZ4InflatingStreamBuf &&) = delete;
-    LZ4InflatingStreamBuf(std::istream & istr);
+  LZ4InflatingStreamBuf(const LZ4InflatingStreamBuf &) = delete;
+  LZ4InflatingStreamBuf(LZ4InflatingStreamBuf &&) = delete;
+  LZ4InflatingStreamBuf &operator=(const LZ4InflatingStreamBuf &) = delete;
+  LZ4InflatingStreamBuf &operator=(LZ4InflatingStreamBuf &&) = delete;
+  LZ4InflatingStreamBuf(std::istream &istr);
+  /// Creates an InflatingInputStream for expanding the compressed data read
+  /// from the given input stream.
 
-    ~LZ4InflatingStreamBuf();
+  ~LZ4InflatingStreamBuf();
+  /// Destroys the InflatingInputStream.
 
-    void reset() { }
+  // int close();
 
+  /// Finishes up the stream.
+  ///
+  /// Must be called when inflating to an output stream.
+
+  void reset()  {
+  }
+
+  /// Resets the stream buffer.
 protected:
     int readFromDevice(char * buffer, std::streamsize length);
+    // int writeToDevice(const char * buffer, std::streamsize length);
+    // int sync();
 
 private:
-    std::istream * pIstr;
-
-    /// place to keep incoming data
+    std::istream*  pIstr;
     std::vector<char> compressedBuffer;
-    char * src_buffer;
-    enum {
-        /// proper buffer sizes are not required from functional standpoint, but should speed up processing
-        STREAM_BUFFER_SIZE = (256 * 1024) + 4096, // CH uses 256k frames
-        INFLATE_BUFFER_SIZE = 256 * 1024 * 4      // lets estimate ratio is 4
+    enum
+    {
+        STREAM_BUFFER_SIZE  = 1024,
+        INFLATE_BUFFER_SIZE = 32768
     };
-    LZ4F_dctx * dctx;
-    size_t bytes_left = 0;
+    LZ4F_dctx* dctx;
+    // char*    _buffer;
+    // std::istream*  _pIstr;
+    // std::ostream*  _pOstr;
+    // // z_stream _zstr;
+    // bool     _eof;
+    // bool     _check;
+
+    // uint16_t     avail_in;  /* number of bytes available at next_in */
+    // uint32_t    total_in;  /* total number of input bytes read so far */
+
+    // unsigned char    *next_out; /* next output byte will go here */
+    // unsigned char    *next_in; /* next output byte will go here */
+    // uint16_t     avail_out; /* remaining free space at next_out */
+    // uint32_t    total_out; /* total number of bytes output so far */
+  size_t bytes_left = 0;
+  char   * src_buffer;
 };
 
 
-/// Theses classes are needed to ensure the correct initialization
-/// order of the stream buffer and base classes.
-/// Exacltly the same as Poco::InflatingIOS and Poco:InflatingInputStream
-template <typename StreamBufType>
-class GenericInflatingIOS : public virtual std::ios {
+class LZ4InflatingIOS: public virtual std::ios
+	/// The base class for InflatingOutputStream and InflatingInputStream.
+	///
+	/// This class is needed to ensure the correct initialization
+	/// order of the stream buffer and base classes.
+{
 public:
-    GenericInflatingIOS(std::istream & istr) : buf(istr) {
-        poco_ios_init(&buf);
-    }
+	LZ4InflatingIOS(std::istream& istr);
+		/// Creates an LZ4InflatingIOS for expanding the compressed data read from
+		/// the given input stream.
+		///
+		/// Please refer to the zlib documentation of inflateInit2() for a description
+		/// of the windowBits parameter.
 
+	~LZ4InflatingIOS();
+		/// Destroys the LZ4InflatingIOS.
 
-    ~GenericInflatingIOS() {};
+	LZ4InflatingStreamBuf* rdbuf();
+		/// Returns a pointer to the underlying stream buffer.
 
 protected:
-    StreamBufType buf;
+	LZ4InflatingStreamBuf _buf;
 };
 
-
-using LZ4InflatingIOS = GenericInflatingIOS<LZ4InflatingStreamBuf>;
-
-
-template <typename IOSType>
-class GenericInflatingInputStream : public std::istream, public IOSType {
+class LZ4InflatingInputStream: public std::istream, public LZ4InflatingIOS
+{
 public:
-    using IOSType::buf;
-    GenericInflatingInputStream(std::istream & istr) : std::istream(&buf), IOSType(istr) { }
+	LZ4InflatingInputStream(std::istream& istr);
+		/// Creates an InflatingInputStreamLZ4 for expanding the compressed data read from
+		/// the given input stream.
+		///
+		/// Please refer to the zlib documentation of inflateInit2() for a description
+		/// of the windowBits parameter.
 
-    ~GenericInflatingInputStream() { }
+	~LZ4InflatingInputStream();
+		/// Destroys the InflatingInputStreamLZ4.
+
+	void reset();
+		/// Resets the zlib machinery so that another zlib stream can be read from
+		/// the same underlying input stream.
 };
-
-using LZ4InflatingInputStream = GenericInflatingInputStream<LZ4InflatingIOS>;
