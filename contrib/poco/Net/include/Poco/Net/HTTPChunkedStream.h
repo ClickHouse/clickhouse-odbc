19d5987b07aa713683b56e38d2e55bd23da0f8a6
Andrew Slabko <andrew@slabko.com>
2025-10-27 23:04:17 +0100

    Add prefetch buffer for ClickHouse exceptions
    This approach differs from the previous implementation, which relied on the
    assumption that the AmortizedIStreamReader would read enough data to include
    the exception message. However, this is not always guaranteed, for example,
    part of the exception message might be read without reaching EOF. In such cases,
    the data parser could incorrectly interpret the __exception__ marker as part of
    the regular data. With the look-ahead buffer, this issue is eliminated because
    some data is always prefetched but not processed until the next batch is read.
    
    Additionally, this buffer help with implementing data decompression in future.

--- contrib/poco/Net/include/Poco/Net/HTTPChunkedStream.h
+++ contrib/poco/Net/include/Poco/Net/HTTPChunkedStream.h
@@ -25,11 +25,12 @@
 #include <cstddef>
 #include <istream>
 #include <ostream>
+#include <optional>
 
 // --------------------------- WARNING LOCAL CHANGES ------------------------------- //
 // This class has been heavily modified to resolve issues related to incomplete
-// streams of data, i.e., when the server closes the connection before sending all
-// data.
+// data streams — specifically, cases where the server closes the connection
+// before sending all the data.
 // See https://github.com/pocoproject/poco/issues/5032
 //
 // Additionally, it implements handling of ClickHouse mid-stream exceptions.
@@ -37,51 +38,32 @@
 //
 // Unfortunately, due to the design of std::stream, an exception prevents the
 // transmission of data that was successfully read before the exception occurred.
-// Therefore, when an unexpected connection close occurs, the exception message
+// Therefore, when an unexpected connection closure occurs, the exception message
 // from the stream will most likely never reach the client.
 //
-// For simplicity and predictability of mid-stream exception handling, this logic
-// has also been implemented in this class. To achieve that, the last 32 KB of data
-// is always cached in a lookback circular buffer. If the connection closes before
-// the end of the stream, the buffer is checked for the exception marker.
+// For simplicity and predictability in mid-stream exception handling, this logic
+// has also been implemented in this class. To achieve that, the class always
+// prefetches extra data equal to the assumed maximum exception size. The class
+// then always serves data from the prefetch buffer. Additionally, if the requested
+// data size is small, the class fetches more data into the buffer to avoid extra
+// read calls from the socket.
+//
+// In cases where the socket is closed without receiving the terminating
+// zero-sized chunk, the class checks the contents of the prefetch buffer for
+// the marker `__exception__\r\n`. If found, it throws a corresponding exception
+// with the message that follows the marker.
 // --------------------------------------------------------------------------------- //
 
 namespace Poco {
 namespace Net {
 
+POCO_DECLARE_EXCEPTION(Net_API, IncorrectSize, NetException)
 POCO_DECLARE_EXCEPTION(Net_API, IncompleteChunkedTransfer, NetException)
 POCO_DECLARE_EXCEPTION(Net_API, IncorrectChunkSize, NetException)
 POCO_DECLARE_EXCEPTION(Net_API, ClickHouseException, NetException)
 
 class HTTPSession;
 class MessageHeader;
-
-class LookbackBuffer
-    /// Circular buffer that maintains the last `buffer_size` bytes of a stream.
-{
-public:
-	explicit LookbackBuffer(size_t buffer_size);
-
-    /// commit data to the buffer
-	void commit(const char * data, size_t data_size);
-
-    /// Get last `buffer_size` committed to the buffer.
-    /// If there were less than `buffer_size` bytes committed, the
-    /// all the data will be returned. Must be used in conjunction
-    /// with `size()`
-	const char * data();
-
-    /// Get the size of data stored in the buffer. If less than
-    /// `buffer_size` bytes were committed to the buffer, the
-    /// number of committed bytes will be returned, otherwise
-    /// it will `buffer_size`.
-	[[nodiscard]] size_t size() const;
-
-private:
-	std::vector<char> buffer;
-	size_t count{0};
-	size_t head{0};
-};
 
 class Net_API HTTPChunkedStreamBuf: public HTTPBasicStreamBuf
 	/// This is the streambuf class used for reading and writing
@@ -92,22 +74,51 @@ public:
 
 	HTTPChunkedStreamBuf(HTTPSession& session, openmode mode, MessageHeader* pTrailer = nullptr);
 	~HTTPChunkedStreamBuf();
+
+	// Sends the terminating bytes if this is a write buffer.
 	void close();
 
 protected:
-	int readFromDevice(char* buffer, std::streamsize length);
-	int writeToDevice(const char* buffer, std::streamsize length);
+	// Returns data from the prefetch buffer.
+	// Public function only wraps exceptions. The real work is done
+	// in private `readFromDeviceImpl`
+	int readFromDevice(char* buffer, std::streamsize length) override;
+
+	// Writes data to the socket (this function has not been modified).
+	int writeToDevice(const char* buffer, std::streamsize length) override;
 
 private:
-	int readChar();
-	int readChunkEncodedStream(char* buffer, std::streamsize length);
+	int readFromDeviceImpl(char* buffer, std::streamsize length);
+
+	// Ensures that the prefetch buffer contains enough bytes to check
+	// for possible ClickHouse exceptions.
+	void prefetch(std::streamsize length);
+
+	// Reads `length` bytes of data from the socket.
+	// Throws IncompleteChunkedTransfer if EOF is encountered.
+	int readDataFromSocket(char* buffer, std::streamsize length);
+
+	// Reads a single character from the socket.
+	// Throws IncompleteChunkedTransfer if EOF is encountered.
+	int readCharFromSocket();
+
+	// Checks whether the prefetch buffer contains a ClickHouse exception.
+	std::optional<ClickHouseException> checkForClickHouseException();
+
+	// Resets the prefetch buffer.
+	void reset();
 
 	HTTPSession&    _session;
 	openmode        _mode;
 	std::streamsize _chunk;
 	std::string     _chunkBuffer;
 	MessageHeader*  _pTrailer;
-	LookbackBuffer  _lookback_buffer;
+
+	std::vector<char> _prefetchBuffer;
+	size_t _prefetchBufferSize; // Amount of data available in the buffer.
+	size_t _prefetchBufferHead; // Current read position in the buffer.
+	bool _eof;                  // True if no more data is available in the socket.
+
 };
 
 
